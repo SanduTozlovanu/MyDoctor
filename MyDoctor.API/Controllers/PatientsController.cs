ae8ab034d60ee14a013eb7bd1847b3857da7f95f
Nae Theodor-Gabriel <73588948+ntheodorg@users.noreply.github.com>
2022-11-23 18:55:42 +0200

    Update PatientsController.cs

--- MyDoctor.API/Controllers/PatientsController.cs
+++ MyDoctor.API/Controllers/PatientsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyDoctor.API.DTOs;
 using MyDoctor.Domain.Models;
 using MyDoctorApp.Infrastructure.Generics;
 using MyDoctorApp.Infrastructure.Generics.GenericRepositories;
@@ -11,10 +12,41 @@ namespace MyDoctor.API.Controllers
     public class PatientsController : ControllerBase
     {
         private readonly IRepository<Patient> patientsRepository;
+        private readonly IRepository<MedicalHistory> medicalHistoryRepository;
 
-        public PatientsController(IRepository<Patient> patientsRepository)
+        public PatientsController(IRepository<Patient> patientsRepository, IRepository<MedicalHistory> medicalHistoryRepository)
         {
             this.patientsRepository = patientsRepository;
+            this.medicalHistoryRepository = medicalHistoryRepository;
+        }
+
+        [HttpPost("{patientId:guid}/{medicalHistoryId:guid}")]
+        public IActionResult RegisterMedicalHistory(Guid patientId, Guid medicalHistoryId)
+        {
+            var patient = patientsRepository.Get(patientId);
+            var medicalHistory = medicalHistoryRepository.Get(medicalHistoryId);
+
+            if (patient == null || medicalHistory == null)
+            {
+                return NotFound();
+            }
+
+            patient.RegisterMedicalHistory(medicalHistory);
+
+
+            medicalHistoryRepository.SaveChanges();
+            patientsRepository.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreatePatientDto dto)
+        {
+            var patient = new Patient(dto.Email, dto.Password, dto.FirstName, dto.LastName, dto.Age);
+            patientsRepository.Add(patient);
+            patientsRepository.SaveChanges();
+            return Created(nameof(Get), patient);
         }
 
         [HttpGet]
