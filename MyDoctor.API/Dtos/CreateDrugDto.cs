07fd047872ae1bff0505a3c09d8e167940cb3648
Tozlovanu Sandu <77733069+SanduTozlovanu@users.noreply.github.com>
2022-11-23 17:50:56 +0200

    Dtos folder created

---
+++ MyDoctor.API/Dtos/CreateDrugDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MyDoctor.API.Dtos
+{
+    public class CreateDrugDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public uint Quantity { get; set; }
+    }
+}
