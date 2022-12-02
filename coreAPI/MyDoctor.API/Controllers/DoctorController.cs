dd24ca67a7188ef0722555a39c1f31594d764a4a
Tozlovanu Sandu <77733069+SanduTozlovanu@users.noreply.github.com>
2022-12-02 12:19:31 +0200

    Upload photo in create doctor endpoint finished, (TODO -  change doctor domain to save profilePhotoName and diplomaPhotoName in Database)

--- coreAPI/MyDoctor.API/Controllers/DoctorController.cs
+++ coreAPI/MyDoctor.API/Controllers/DoctorController.cs
@@ -29,7 +29,7 @@ namespace MyDoctor.API.Controllers
             return Ok(doctorsRepository.All().Select(d => new DisplayDoctorDto(d.Id, d.MedicalRoomId, d.Email, d.Speciality, d.FirstName, d.LastName)));
         }
         [HttpPost]
-        public IActionResult Create([FromBody] CreateDoctorDto dto)
+        public IActionResult Create([FromForm] CreateDoctorDto dto)
         {
             List<MedicalRoom> medicalRooms = medicalRoomRepository.All().ToList();
             (MedicalRoom?, int) medicalRoomWithFewestDoctors = new(null, int.MaxValue);
@@ -66,6 +66,29 @@ namespace MyDoctor.API.Controllers
             string hashedPassword = AccountInfoManager.HashPassword(dto.UserDetails.Password);
             var doctor = new Doctor(dto.UserDetails.Email, hashedPassword, dto.UserDetails.FirstName, dto.UserDetails.LastName, dto.Speciality);
             medicalRoom.RegisterDoctors(new List<Doctor> { doctor });
+
+            if (dto.ProfilePhoto == null)
+            {
+                return BadRequest("No profile photo has been loaded.");
+            }
+            if (dto.DiplomaPhoto == null)
+            {
+                return BadRequest("No diploma photo has been loaded.");
+            }
+            try
+            {
+                var profilePhotoPath = Path.Combine(Directory.GetCurrentDirectory(), "profilePhotos", dto.ProfilePhoto.FileName);
+                var diplomaPhotoPath = Path.Combine(Directory.GetCurrentDirectory(), "diplomaPhotos", dto.DiplomaPhoto.FileName);
+
+                var profileStream = new FileStream(profilePhotoPath, FileMode.Create);
+                var diplomaStream = new FileStream(diplomaPhotoPath, FileMode.Create);
+                dto.ProfilePhoto.CopyToAsync(profileStream);
+                dto.DiplomaPhoto.CopyToAsync(diplomaStream);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error occured during processing the images - {ex}.");
+            }
 
             doctorsRepository.Add(doctor);
             doctorsRepository.SaveChanges();
