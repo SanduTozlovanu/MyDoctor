e7824d9c4b3ffc5b0a0a6f2c77021c1915f173cb
Teodora3120 <vestale.teodora00@gmail.com>
2023-01-04 16:37:43 +0200

    feat: add some new pages

--- website/src/api/appointments.js
+++ website/src/api/appointments.js
@@ -1,9 +1,15 @@
 import axios from './index'
 
 class AppointmentApi {
- static async CreateAppointment(patientId, doctorId, data){
-    return await axios.post(`/Appointments/${patientId}_${doctorId}/create_appointment`, data)
- }
+  static async CreateAppointment(patientId, doctorId, data) {
+    return await axios.post(
+      `/Appointments/${patientId}_${doctorId}/create_appointment`,
+      data,
+    )
+  }
+  static async GetAppointments(doctorId) {
+    return await axios.get(`/Appointments/${doctorId}`)
+  }
 }
 
 export default AppointmentApi
