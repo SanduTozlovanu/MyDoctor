9f16b877db0e8841dfd9829c09c41ac524bb1ce3
Tozlovanu Sandu <77733069+SanduTozlovanu@users.noreply.github.com>
2023-01-09 14:22:26 +0200

    Code smell fix + isStatusOk function fix.

--- coreAPI/MyDoctor.Application/Exceptions/MapperExceptions.cs
+++ coreAPI/MyDoctor.Application/Exceptions/MapperExceptions.cs
@@ -1,7 +1,12 @@
-﻿namespace MyDoctor.Application.Exceptions
+﻿using System.Runtime.Serialization;
+
+namespace MyDoctor.Application.Exceptions
 {
+    [Serializable]
     public class BaseMapperException : Exception
     {
         public BaseMapperException() : base("Issue with the mapper") { }
+        protected BaseMapperException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
     }
 }
